c3076a70f514df0c3b0ce81bd622bcbd125a35f7
Alfr3XD <87869485+Alfr3XD@users.noreply.github.com>
2022-07-24 22:06:28 -0500

    Types -MemberCard

--- sources/typings/main.d.ts
+++ sources/typings/main.d.ts
@@ -20,6 +20,18 @@ export function fillRoundRect(ctx: CanvasRenderingContext2D, x: number, y: numbe
  */
 export function abbreviateNumber(value: number): string;
 
+/**
+ * 
+ * @param ctx Contexto de canvas
+ * @param x Eje X [`Derecha o Izquierda`] del inicio de tu rectángulo`
+ * @param y Eje Y [`Arriba o Abajo`] del inicio de tu rectángulo`
+ * @param w Anchura de tu circuferencia
+ * @param h Altura de tu circuferencia
+ * @param f ¿Quieres que tu circuferencia sea rellenado?
+ * @param s ¿Quieres que tu circuferencia tenga contorno?
+ */
+export function circleImage(ctx: CanvasRenderingContext2D, x: number, y: number, h: number, w: number, f?: boolean, s?: boolean): void
+
 // Level Card
 export type CardLevelData = {
     username?: string,
@@ -114,4 +126,104 @@ export class LevelCard {
     public setBackground(background: string, blur: number): this
     /** Construye la tarjeta de niveles */
     public render(): Promise<Buffer>    
+}
+
+export type CardMemberData = {
+    username?: string,
+    title?: string,
+    description?: string,
+    memberCount?: string,
+    colors?: {
+        title: string,
+        description: string,
+        username: string,
+        stroke: string,
+        box: string,
+        memberCount: string
+    }
+    avatar?: string,
+    background?: string,
+    blur?: number,
+    radius?: number,
+    box?: boolean,
+    fonts?: {
+        usernameFont: string,
+        titleFont: string,
+        descriptionFont: string,
+        memberCountFont: string
+    }
+}
+
+/**
+ * Modifica una tarjeta de miembro de Discord
+ * ```js
+ * const card = new MemberCard()
+ * .setUsername("User#0000")
+ * 
+ * card.render()
+ * .then((buffer) => {
+ *  attachment.file(buffer, "image.png");
+ * });
+ * ```
+ */
+export class MemberCard {
+    /**
+     * Los datos se pueden ingresar si lo deseas en forma de [object]
+     * @param data Recomiendo hacerlo con los métodos.
+     */
+    constructor(data?: CardMemberData);
+    /**
+    * @param font Datos de la fuente de letra
+    */
+    public registerFont(font: {path: string, options: { family: string, weight?: string, style?: string }}[]): this
+
+    /**
+     * @param username nombre del usuario
+     * @param color color del nombre del usuario
+     * @param font Fuente de letra
+     */
+     setUsername(username: string, color?: string, font?: string): this
+
+    /**
+     * @param title Título de la tarjeta
+     * @param color Color del título
+     * @param font Fuente de letra
+     */
+    setTitle(title:string, color?: string, font?: string): this
+
+    /**
+     * @param description Descripción de la tarjeta
+     * @param color Color de la descripción
+     * @param font Fuente de letra
+     */
+    setDescription(description:string, color?: string, font?: string): this
+
+    /**
+     * @param memberCount Miembros del servidor
+     * @param color Color del texto
+     * @param font Fuente de letra
+     */
+    setMemberCount(memberCount:string, color?: string, font?: string): this
+
+    /**
+     * @param background url de la imagen de fondo
+     * @param blur cambia el desenfocado de la imágen de fondo
+     * @param radius cambia el radio de la imágen de fondo
+     */
+    setBackground(background: string, blur: number, radius: number): this
+
+    /**
+     * @param avatar url de la imagen de avatar
+     * @param color color del borde de la imagen de avatar
+     */
+     setAvatar(avatar: string, color: string): this
+
+    /**
+     * @param box activa o desactiva el borde de la tarjeta 
+     * @param color color de la caja
+     */
+    setBox(box: boolean, color: string): this
+
+    /** Construye la tarjeta de niveles */
+    public render(): Promise<Buffer>  
 }
